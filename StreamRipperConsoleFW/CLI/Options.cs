7f3e1a57ca1bd12c0798982dd199b83fc065311a
Randall Flagg <flagg1980@outlook.com>
2019-06-06 21:49:36 +0200

    Added CLI for record duration

---
+++ StreamRipperConsoleFW/CLI/Options.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace StreamRipperConsoleFW.CLI
+{
+    internal class Options
+    {
+        [Option('d', "duration", Required = true, HelpText = "Time in minutes to keep recording.")]
+        public int Duration { get; set; }
+    }
+}
--- StreamRipperConsoleFW/Program.cs
+++ StreamRipperConsoleFW/Program.cs
@@ -1,6 +1,9 @@
-﻿using StromReisser3000.Domain;
+﻿using CommandLine;
+using StreamRipperConsoleFW.CLI;
+using StromReisser3000.Domain;
 using StromReisser3000.Interfaces;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading;
 
@@ -11,10 +14,15 @@ namespace StreamRipperConsole
         private static IStromReisser myRipper;
         private static IStromAufzeichner myRecorder;
         private static StreamSource myStreamSource;
+        private static Options options;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => options = opts)
+                .WithNotParsed<Options>(errs => System.Environment.Exit(-1));
 
             myStreamSource = new StreamSource
             {
@@ -26,11 +34,21 @@ namespace StreamRipperConsole
             myRipper = new Mp3StromReisser();
             myRipper.StartRip(myStreamSource);
 
-            var filePath = Path.Combine(@"P:\tmp", "bla.mp3");
-            myRecorder = new Mp3StromAufzeichner((Mp3StromReisser)myRipper, filePath);
+            var now = DateTime.Now;
+            var filePrefix = "Energy_Mastermix";
+            var fileName = $"{filePrefix}_{now.Day:00}-{now.Month:00}-{now.Year}_{now.Hour:00}-{now.Minute:00}-{now.Second:00}.mp3";
+            var fileLocation = Path.Combine(@"P:\tmp", fileName);
+
+            myRecorder = new Mp3StromAufzeichner((Mp3StromReisser)myRipper, fileLocation);
             myRecorder.StartRecord();
 
-            Thread.Sleep(5000);
+            var sw = new Stopwatch();
+            sw.Start();
+            do
+            {
+                Thread.Sleep(1000);
+            }
+            while (sw.Elapsed.TotalMinutes < options.Duration);
 
             myRecorder.StopRecord();
             myRipper.StopRip();
